702a40eed9e5d9909b07ed3df3ed1d7cae935542
marioluigipros <marioprospero@outlook.it>
2015-04-08 14:38:19 +0200

    Secondo commit

--- EserciziTest/Esercizi.cs
+++ EserciziTest/Esercizi.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EserciziTest
+namespace Esercizi
 {
-    class Esercizi
+    public class Esercizi
     {
+        public decimal SommaMultipliNnumeriMinoriDi(int[] numeri, int limite)
+        {
+            return 0;
+        }
+
+        public int[] CompilaVettore(int[] vettore)
+        {
+            return null;
+        }
     }
 }
--- EserciziTests/EserciziTestsClass.cs
+++ EserciziTests/EserciziTestsClass.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Core;
+using NUnit.Framework;
+
 
 namespace EserciziTests
 {
+    [TestFixture]
     public class EserciziTestsClass
     {
+        [Test]
+        public void Check2And3MultiplesLessThen10()
+        {
+
+        }
+
+        [Test]
+        public void CheckVectorOf5NumsForEachPossibleSeed()
+        {
+
+        }
     }
 }
